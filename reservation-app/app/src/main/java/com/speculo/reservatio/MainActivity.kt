7263984b9e40ec5dc199941471827e18ba3ec8d1
vandit vasa <vasavandit@gmail.com>
2022-08-05 23:43:08 +0530

    added navigation component

--- reservation-app/app/src/main/java/com/speculo/reservatio/MainActivity.kt
+++ reservation-app/app/src/main/java/com/speculo/reservatio/MainActivity.kt
@@ -2,10 +2,12 @@ package com.speculo.reservatio
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import com.speculo.reservatio.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        val binding: ActivityMainBinding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
     }
 }
