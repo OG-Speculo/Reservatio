c10d32fbade63dee44f8784dfda9659f9b7f6e92
vandit vasa <vasavandit@gmail.com>
2022-08-06 02:54:25 +0530

    fragment_scan.xml implemented and added drawables

---
+++ reservation-app/app/src/main/java/com/speculo/reservatio/ScanFragment.kt
@@ -0,0 +1,24 @@
+package com.speculo.reservatio
+
+import android.os.Bundle
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import androidx.databinding.DataBindingUtil
+import androidx.fragment.app.Fragment
+import com.google.firebase.database.R
+import com.speculo.reservatio.databinding.FragmentScanBinding
+
+class ScanFragment : Fragment() {
+
+    lateinit var binding: FragmentScanBinding
+
+    override fun onCreateView(
+        inflater: LayoutInflater,
+        container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? {
+        binding = DataBindingUtil.inflate(inflater, R.layout.fragment_scan, container, false)
+        return binding.root
+    }
+}
