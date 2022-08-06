283347dbfd6cc2fe392c5f49b726706e6e4e6556
vandit vasa <vasavandit@gmail.com>
2022-08-07 00:08:47 +0530

    FinalFragment.kt backend done

--- reservatio-app/app/src/main/java/com/speculo/reservatio/FinalFragment.kt
+++ reservatio-app/app/src/main/java/com/speculo/reservatio/FinalFragment.kt
@@ -1,6 +1,8 @@
 package com.speculo.reservatio
 
+import android.content.Context
 import android.os.Bundle
+import android.util.Log
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
@@ -17,5 +19,24 @@ class FinalFragment : Fragment() {
     ): View? {
         binding = DataBindingUtil.inflate(inflater, R.layout.fragment_final, container, false)
         return binding.root
+    }
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+
+        val sharedPref = activity?.getPreferences(Context.MODE_PRIVATE) ?: return
+        val restaurant = sharedPref.getString("restaurant", null)
+        Log.d("TAG", restaurant.toString())
+        binding.apply {
+            restaurantTV.text = restaurant
+
+            homeBTN.setOnClickListener {
+                fragmentManager?.beginTransaction()?.replace(R.id.nav_host_fragment, ScanFragment())?.commit()
+            }
+        }
+        with(sharedPref.edit()){
+            putString("restaurant", null)
+            apply()
+        }
     }
 }
